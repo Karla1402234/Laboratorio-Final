6f08d4274350be94476155e2e4b8a68f1fe80687
Eliza <vanessateban077@gmail.com>
2026-06-12 15:40:34 -0600

    Cliente

---
+++ src/modelo/LaboratorioFinal.java
@@ -0,0 +1,20 @@
+package modelo;
+
+public class LaboratorioFinal {
+
+    int id;
+    String nombre;
+    String telefono;
+
+    public LaboratorioFinal(int id, String nombre, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public void mostrar() {
+        System.out.println("ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Telefono: " + telefono);
+    }
+}
