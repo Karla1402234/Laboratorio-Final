52ff9f37e926b99b75eff2629e3bf84c43dff55f
Eliza <vanessateban077@gmail.com>
2026-06-12 16:09:07 -0600

    cliente agregado

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,52 +1,20 @@
 package modelo;
 
 public class Cliente {
-    private int idCliente;
-    private String nombre;
-    private String direccion;
-    private String telefono;
 
-    public Cliente(int id, String nombre, String direccion, String tel) {
-        this.idCliente = id;
+    int id;
+    String nombre;
+    String telefono;
+
+    public Cliente(int id, String nombre, String telefono) {
+        this.id = id;
         this.nombre = nombre;
-        this.direccion = direccion;
-        this.telefono = tel;
-    }
-
-    public int getIdCliente() {
-        return idCliente;
-    }
-
-    public void setIdCliente(int idCliente) {
-        this.idCliente = idCliente;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    @Override
-    public String toString() {
-        return nombre;
+    public void mostrar() {
+        System.out.println("ID: " + id);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Telefono: " + telefono);
     }
 }
